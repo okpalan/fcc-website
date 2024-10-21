ca6483d6634335686b3f153c01b4d39a24584368
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-10-21 19:26:57 +0100

    completed template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import sass from 'sass';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(),
+    sass({
+      
+    })
+  ]
 });
